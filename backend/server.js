c888d04f9978b46232b3e449e3ee2d65dee813ee
imjay05 <imjaydigambarshelke@gmail.com>
2026-05-08 20:28:49 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,7 @@ const __dirname = path.resolve();
 app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ limit: "10mb", extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: "http://localhost:5173", credentials: true }));
+app.use(cors({ origin: process.env.FRONTEND_URL || "http://localhost:5173", credentials: true }));
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
