9ff9dae62d0218c85ff85ad701aea55c00f7645d
imjay05 <imjaydigambarshelke@gmail.com>
2026-05-08 20:53:52 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -29,9 +29,9 @@ app.use("/api/vibes", vibeRoutes);
 
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
-  });
+  app.get("/{*path}", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
+});
 }
 
 
